5a57eec717a73b05d3eee7a89c5a4d4d1335f81b
John Matthews <johnmatthews2634@gmail.com>
2018-01-07 14:54:33 -0800

    add form to view

--- mysite/photo_upload/models.py
+++ mysite/photo_upload/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Photo(models.Model):
-    photo = models.FileField(upload_to='photo')
+class PhotoUpload(models.Model):
+    photo = models.FileField(upload_to='PhotoUpload/')
--- mysite/photo_upload/views.py
+++ mysite/photo_upload/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the photo_upload index.")
+    template = loader.get_template('photo_upload/index.html')
+    return HttpResponse(template.render(context, request))
