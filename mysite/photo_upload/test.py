93d2b7535c7f29da78ed9fe313e88c22b61953e4
John Matthews <johnmatthews2634@gmail.com>
2018-01-08 14:06:32 -0800

    test script development

---
+++ mysite/photo_upload/test.py
@@ -0,0 +1,5 @@
+
+
+
+def test_function(request):
+    return request.method == 'POST'
--- mysite/photo_upload/views.py
+++ mysite/photo_upload/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files import File
+from . import test
 # from PIL import Image
 @csrf_exempt
 def mainview(request):
+    print (request)
     return render(request, 'photo_upload/mainview.html')
 def img(request):
-    print (request.method)
     photo = request.FILES
+    # print (test.test_function(request)
     return HttpResponse(photo.getlist('img')[0].read(), content_type="image/jpeg")
