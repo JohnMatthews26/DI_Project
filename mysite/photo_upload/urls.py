43802edeb8b299487a0a51171ba6c92c6f965037
John Matthews <johnmatthews2634@gmail.com>
2018-01-07 20:44:48 -0800

    add second view

--- mysite/photo_upload/urls.py
+++ mysite/photo_upload/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.mainview, name='mainview'),
+    path('img', views.img, name='img'),
 ]
--- mysite/photo_upload/views.py
+++ mysite/photo_upload/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.template import loader
-
-def index(request):
-    template = loader.get_template('photo_upload/index.html')
-    return HttpResponse(template.render(context, request))
+from django.views.decorators.csrf import csrf_exempt
+# from PIL import Image
+@csrf_exempt
+def mainview(request):
+    return render(request, 'photo_upload/img.html')
+def img(request, photo):
+    user_upload = request.POST.get('img', False)
+    if user_upload:
+        print ('it worked')
+    else:
+        return render(request, 'photo_upload/mainview.html')
