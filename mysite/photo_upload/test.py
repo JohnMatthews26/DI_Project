65439936ec69de96fcc841907833e50764c155e6
John Matthews <johnmatthews2634@gmail.com>
2018-01-11 14:43:04 -0800

    CI function

--- mysite/photo_upload/test.py
+++ mysite/photo_upload/test.py
@@ -1,5 +1,9 @@
 from django.test import Client
 
 
+
 def test_function(request, response):
+    c = Client()
+    response = c.get('')
+    print (response.content)
     return request.method == 'POST', response
